ef5419434ca01634e24d3a754c9363d246b5e689
vedidit <47222512+vedidit@users.noreply.github.com>
2019-02-10 01:31:06 +0530

    Signal added

--- machine_learning/chatbot/models.py
+++ machine_learning/chatbot/models.py
@@ -1,13 +1,12 @@
 from django.db import models
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 100)
     question_id = models.IntegerField(primary_key = True)
-
-
-
     def __str__(self):
         return self.question_text
 
@@ -17,3 +16,11 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.answer_text
+
+def creating_answer_function(sender,**kwargs):
+    if kwargs['created']:
+       answer = "hi"
+       print("hi")
+       obj = Answer.objects.create(answer_id = kwargs['instance'].question_id, answer_text = answer)
+       obj.save()
+post_save.connect(creating_answer_function,sender = Question)
